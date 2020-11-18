3fb1830c8e5e0ce8a51924d2cc3ef389b09df618
musaddiq <musaddiq@exarotech.com>
2020-11-18 07:30:24 +0100

    working changes

--- aspnet-core/src/E_Ticketer.EntityFrameworkCore/Migrations/20201118061622_modification 18112020.Designer.cs
+++ aspnet-core/src/E_Ticketer.EntityFrameworkCore/Migrations/20201118061622_modification 18112020.Designer.cs
@@ -4,14 +4,16 @@ using E_Ticketer.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace E_Ticketer.Migrations
 {
     [DbContext(typeof(E_TicketerDbContext))]
-    partial class E_TicketerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201118061622_modification 18112020")]
+    partial class modification18112020
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
