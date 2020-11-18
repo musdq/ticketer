0b81f41a52cd83cd5e67e103750a85634b6c030c
musaddiq <musaddiq@exarotech.com>
2020-11-18 22:59:22 +0100

    fixed database

---
+++ aspnet-core/src/E_Ticketer.EntityFrameworkCore/Migrations/20201118215826_fixing models.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace E_Ticketer.Migrations
+{
+    public partial class fixingmodels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+             name: "AppStations",
+             columns: table => new
+             {
+                 Id = table.Column<int>(type: "int", nullable: false)
+                     .Annotation("SqlServer:Identity", "1, 1"),
+                 CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                 CreatorUserId = table.Column<long>(type: "bigint", nullable: true),
+                 DeleterUserId = table.Column<long>(type: "bigint", nullable: true),
+                 DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                 IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                 LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                 LastModifierUserId = table.Column<long>(type: "bigint", nullable: true),
+                 Lga = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                 Name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                 State = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                 TenantId = table.Column<int>(type: "int", nullable: false)
+             },
+             constraints: table =>
+             {
+                 table.PrimaryKey("PK_AppStations", x => x.Id);
+             });
+
+            migrationBuilder.CreateTable(
+                name: "AppTickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeleterUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierUserId = table.Column<long>(type: "bigint", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    TenantId = table.Column<int>(type: "int", nullable: false),
+                    TicketType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppTickets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppTrains",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeleterUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Identifier = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierUserId = table.Column<long>(type: "bigint", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    TenantId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppTrains", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppTrips",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ArrivalTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeleterUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DepartureTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DestStationId = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierUserId = table.Column<long>(type: "bigint", nullable: true),
+                    MaxOtherTickets = table.Column<int>(type: "int", nullable: false),
+                    MaxVipTickets = table.Column<int>(type: "int", nullable: false),
+                    OriginStationId = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    TenantId = table.Column<int>(type: "int", nullable: false),
+                    TrainId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppTrips", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppTrips_AppTrains_TrainId",
+                        column: x => x.TrainId,
+                        principalTable: "AppTrains",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppBookings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BookingType = table.Column<int>(type: "int", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeleterUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierUserId = table.Column<long>(type: "bigint", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    TenantId = table.Column<int>(type: "int", nullable: false),
+                    TicketPrice = table.Column<double>(type: "float", nullable: false),
+                    TicketType = table.Column<int>(type: "int", nullable: false),
+                    TripId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppBookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppBookings_AppTrips_TripId",
+                        column: x => x.TripId,
+                        principalTable: "AppTrips",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppBookings_TripId",
+                table: "AppBookings",
+                column: "TripId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppTrips_TrainId",
+                table: "AppTrips",
+                column: "TrainId");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
